6853c41c25427b509c404b2bb9e439f22e95e249
Maggie Lerman <lerman.maggiel@gmail.com>
2025-08-06 14:51:10 -0400

    Update Header.tsx

--- src/components/navigation/Header.tsx
+++ src/components/navigation/Header.tsx
@@ -207,13 +207,7 @@ const Header = () => {
                     <span>Dashboard</span>
                   </Link>
                 </DropdownMenuItem>
-                <DropdownMenuSeparator />
-                <DropdownMenuItem asChild>
-                  <Link to="/donor/auth" className="cursor-pointer">
-                    <Heart className="mr-2 h-4 w-4" />
-                    <span>Donor Portal</span>
-                  </Link>
-                </DropdownMenuItem>
+
                 <DropdownMenuSeparator />
                 <DropdownMenuItem onClick={handleSignOut} className="cursor-pointer">
                   <LogOut className="mr-2 h-4 w-4" />
